95557735393d68bcfa543b37a890a8c2b8ecc725
sagardwivedi <sagar.dw20@gmail.com>
2025-02-02 23:20:12 +0530

    init

--- backend/app/api/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.routes import login, users
-
-api_router = APIRouter()
-api_router.include_router(login.router)
-api_router.include_router(users.router)
